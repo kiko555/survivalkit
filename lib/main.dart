24e02783658279ff7fa60ac3bc0fcbde25bd041b
kiko555 <kiko555@gmail.com>
2019-01-08 00:11:34 +0800

    1st commit

---
+++ lib/main.dart
@@ -0,0 +1,306 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geocoder/geocoder.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+// void main() => runApp(MyApp());
+
+void main() {
+  openDB();
+
+  /*runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(title: const Text('Google Maps demo')),
+      body: MapsDemo(),
+    ),
+  ));*/
+
+  runApp(MaterialApp(
+    title: 'Navigation Basics',
+    home: FirstScreen(),
+  ));
+}
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Screen'),
+      ),
+      body:
+          /*Center(
+        child: RaisedButton(
+          child: Text('Launch screen'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondScreen()),
+            );
+          },
+        ),
+      ), */
+          Center(
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.map),
+              title: Text('Map'),
+            ),
+            ListTile(
+              leading: Icon(Icons.photo_album),
+              title: Text('Album'),
+            ),
+            ListTile(
+              leading: Icon(Icons.phone),
+              title: Text('Phone'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Screen"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+openDB() async {
+  // Get a location using getDatabasesPath
+  var databasesPath = await getDatabasesPath();
+  print("databasesPath:${databasesPath}");
+
+  String path = join(databasesPath, 'place.db');
+  print("databasesPath With DB name:${path}");
+
+  // try opening (will work if it exists)
+  Database db;
+  try {
+    db = await openDatabase(path, readOnly: true);
+    print("open db ok!");
+  } catch (e) {
+    print("Error $e");
+  }
+
+  if (db == null) {
+    // Should happen only the first time you launch your application
+    print("Creating new copy from asset");
+
+    var xDBpath = await rootBundle.load(join("assets", "place.db"));
+    print("Opening existing database path:${xDBpath}");
+
+    // Copy from asset
+    ByteData data = await rootBundle.load(join("assets", "place.db"));
+    List<int> bytes =
+        data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+    await new File(path).writeAsBytes(bytes);
+
+    // open the database
+    db = await openDatabase(path, readOnly: true);
+  } else {
+    print("Opening existing database");
+  }
+
+  // open the database
+  // Database database = await openDatabase(path, version: 1);
+
+  loadData(db);
+}
+
+loadData(db) async {
+  // Get the records
+  List<Map> list = await db.rawQuery('SELECT * FROM location');
+  return list;
+
+  print(list);
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class MapsDemo extends StatefulWidget {
+  @override
+  State createState() => MapsDemoState();
+}
+
+class MapsDemoState extends State<MapsDemo> {
+  GoogleMapController mapController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(15.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Center(
+            child: SizedBox(
+              width: 300.0,
+              height: 200.0,
+              child: GoogleMap(
+                onMapCreated: _onMapCreated,
+              ),
+            ),
+          ),
+          RaisedButton(
+            child: const Text('Go to London'),
+            onPressed: mapController == null
+                ? null
+                : () async {
+                    // From a query
+                    final query = '彰化縣大村鄉大村村中正西路338號';
+                    var addresses =
+                        await Geocoder.local.findAddressesFromQuery(query);
+                    var first = addresses.first;
+                    mapController.animateCamera(CameraUpdate.newCameraPosition(
+                      CameraPosition(
+                        bearing: 270.0,
+                        //target: LatLng(51.5160895, -0.1294527),
+                        //target: LatLng(23.9937736, 120.5435351),
+                        target: LatLng(first.coordinates.latitude,
+                            first.coordinates.longitude),
+                        tilt: 30.0,
+                        zoom: 17.0,
+                      ),
+                    ));
+                    print("${first.featureName} : ${first.coordinates}");
+                    print("${addresses.cast()}");
+                  },
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    setState(() {
+      mapController = controller;
+    });
+  }
+}
